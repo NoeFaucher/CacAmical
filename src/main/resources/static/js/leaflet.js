530c69e060d369a5c028aef7b5ffab36e6e8eeb2
ducoulombi <ducoulombi@cy-tech.fr>
2023-10-18 17:25:06 +0200

    chore: add openStreetMap and lealfet to manage map

---
+++ src/main/resources/static/js/leaflet.js
@@ -0,0 +1,14 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+var poopIcon = L.icon({
+    iconUrl: '../static/img/poop.png',
+    iconSize: [45, 45],
+});
+
+// Ajoutez un gestionnaire d'événements "click" sur la carte
+map.on('click', function (e) {
+    var marker = L.marker(e.latlng, { icon: poopIcon }).addTo(map);
+    marker.bindPopup("<b>Hello world!</b><br>I am a poop.").openPopup();
+});
