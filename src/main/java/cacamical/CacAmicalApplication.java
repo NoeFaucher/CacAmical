ab3918b95602f5d37f63a766b547ff322aeb3727
NoeFaucher <faucher.noe7@gmail.com>
2023-10-18 14:57:33 +0200

    chore: initiate spring project

---
+++ src/main/java/cacamical/CacAmicalApplication.java
@@ -0,0 +1,13 @@
+package cacamical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacAmicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacAmicalApplication.class, args);
+	}
+
+}
